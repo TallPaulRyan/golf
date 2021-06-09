c225094155bf612dcb85ff9a4dde4e0e179a3809
Paul Ryan <pryan@chime.com>
2021-06-09 15:43:19 -0700

    added delete to course index

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -42,6 +42,12 @@ class CoursesController < ApplicationController
     end
   end
 
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+
+    redirect_to courses_path
+  end
 
   private
 
